ba8c32a3042da28667379f9bcb1507ff40fb2401
X <xndrya@gmail.com>
2019-07-29 22:59:38 +0300

    Тест для логина

--- fixture/session.py
+++ fixture/session.py
@@ -1,40 +1,44 @@
 class SessionHelper:
 
-    def __init__(self,app):
-        self.app=app
+    def __init__(self, app):
+        self.app = app
 
     def login(self, username, password):
         wd = self.app.wd
         self.app.open_home_page()
-        wd.find_element_by_xpath("//input[@name='user']").clear()
-        wd.find_element_by_xpath("//input[@name='user']").send_keys(username)
-        wd.find_element_by_xpath("//input[@name='pass']").clear()
-        wd.find_element_by_xpath("//input[@name='pass']").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def ensure_login(self, username, password):
-        wd = self.app.wd
-        if self.is_logged_in():
-            if self.is_logged_in_as(username):
-                return
-            else:
-                self.logout()
-        self.login(username,password)
+        wd.find_element_by_name("username").clear()
+        wd.find_element_by_name("username").send_keys(username)
+        wd.find_element_by_name("password").clear()
+        wd.find_element_by_name("password").send_keys(password)
+        wd.find_element_by_xpath("//input[@type='submit']").click()
 
     def logout(self):
         wd = self.app.wd
-        wd.find_element_by_xpath("//a[contains(@href, '#')]").click()
-        wd.find_element_by_name("user")
+        wd.find_element_by_link_text("Logout").click()
+        wd.find_element_by_name("username")
 
     def ensure_logout(self):
         wd = self.app.wd
-        if self.is_logged_in():
+        if self.is_logged_in() > 0:
             self.logout()
 
     def is_logged_in(self):
         wd = self.app.wd
-        return len(wd.find_elements_by_link_text("Logout")) > 0
+        return len(wd.find_elements_by_link_text("Logout"))
 
     def is_logged_in_as(self, username):
         wd = self.app.wd
-        return wd.find_element_by_xpath("//*[@id='top']/form/b").text == "("+ username + ")"
+        return self.get_logged_user() == username
+
+    def get_logged_user(self):
+        wd = self.app.wd
+        return wd.find_element_by_css_selector("td.login-info-left span").text
+
+    def ensure_login(self, username, password):
+        wd = self.app.wd
+        if self.is_logged_in() > 0:
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username, password)
