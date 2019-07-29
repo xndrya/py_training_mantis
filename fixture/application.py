2e4549168a28d70426b3317705c730ec3d63b94a
X <xndrya@gmail.com>
2019-07-29 22:55:22 +0300

    Оптимизация conftest и application

--- fixture/application.py
+++ fixture/application.py
@@ -1,37 +1,30 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
-from fixture.group import GroupHelper
-from fixture.contact import ContactHelper
 
 class Application:
-
     def __init__(self, browser, base_url):
         if browser == "firefox":
-            self.wd = webdriver.Firefox(executable_path ="C:\geckodriver\geckodriver.exe")
+            self.wd = webdriver.Firefox()
         elif browser == "chrome":
             self.wd = webdriver.Chrome()
         elif browser == "ie":
-            self.wd = webdriver.Ie
+            self.wd = webdriver.Ie()
         else:
-            raise ValueError("Unrecognized type of browser - %s" % browser)
-        self.wd.implicitly_wait(1)
+            raise ValueError("Unrecognized browser %s" % browser)
         self.session = SessionHelper(self)
-        self.group = GroupHelper(self)
-        self.contact = ContactHelper(self)
+        self.wd.implicitly_wait(3)
         self.base_url = base_url
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
 
     def open_home_page(self):
         wd = self.wd
-        if wd.current_url.endswith("/addressbook") and wd.find_elements_by_xpath("//*[@id='maintable']/tbody/tr[1]/th[2]/a").text == "Last name":
-            return
         wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
-
-    def is_valid(self):
-        try:
-            self.wd.current_url()
-            return True
-        except:
-            return False
