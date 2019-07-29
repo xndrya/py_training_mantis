9c987a5a30273be5dea9106a548faaf2abd1d494
X <xndrya@gmail.com>
2019-07-29 22:56:33 +0300

    Оптимизация conftest и application

--- fixture/application.py
+++ fixture/application.py
@@ -3,16 +3,14 @@ from fixture.session import SessionHelper
 
 class Application:
     def __init__(self, browser, base_url):
-        if browser == "firefox":
-            self.wd = webdriver.Firefox()
-        elif browser == "chrome":
+        if browser == "chrome":
             self.wd = webdriver.Chrome()
         elif browser == "ie":
             self.wd = webdriver.Ie()
         else:
-            raise ValueError("Unrecognized browser %s" % browser)
+            raise ValueError("Unrecognized browser: %s" % browser)
         self.session = SessionHelper(self)
-        self.wd.implicitly_wait(3)
+        self.wd.implicitly_wait(5)
         self.base_url = base_url
 
     def is_valid(self):
