1fe051d29272ce514493632dad96d4195ca1c1d9
X <xndrya@gmail.com>
2019-08-04 20:00:27 +0300

    Добавление предусловий в тест перед удалением проекта

--- test/test_delete_project.py
+++ test/test_delete_project.py
@@ -1,7 +1,10 @@
+from model.project import Project
 import operator
 
 def test_delete_project(app, start, db, json_project):
     project = json_project
+    if len(db.get_project_list()) == 0:
+        app.project.fill_project_form(Project(name="A", description="B"))
     old_project_list = db.get_project_list()
     app.project.delete(project)
     new_project_list = db.get_project_list()
